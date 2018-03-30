4c7d2b8bdfab47614a62869d62b0f34726848557
brittanylei <brittlei48@gmail.com>
2018-03-30 01:38:34 -0700

    edited format

--- Simulation.java
+++ Simulation.java
@@ -27,15 +27,6 @@ public class Simulation{
     return new Job(a, d);
   }
 
-//-----------------------------------------------------------------------------
-//
-// The following stub for function main contains a possible algorithm for this
-// project.  Follow it if you like.  Note that there are no instructions below
-// which mention writing to either of the output files.  You must intersperse
-// those commands as necessary.
-//
-//-----------------------------------------------------------------------------
-
   public static void main(String[] args) throws IOException{
 
 //    1.  check command line arguments
@@ -43,6 +34,7 @@ public class Simulation{
       System.out.println("Usage: Simulation <input file>");
       System.exit(1);
     }
+    
 //    2.  open files for reading and writing
     Scanner in = new Scanner(new File(args[0]));
     PrintWriter report = new PrintWriter(new FileWriter(args[0] + ".rpt"));
@@ -52,6 +44,7 @@ public class Simulation{
     int m = in.nextInt();
     in.nextLine();
     report.println(m + " Jobs:");
+    
 //    3.  read in m jobs from input file
     Queue store = new Queue();
     for (int i = 0; i < m; i++) {
@@ -62,7 +55,7 @@ public class Simulation{
     report.println("***********************************************************");
 //    4.  run simulation with n processors for n=1 to n=m-1  {
     for (int n=1; n<m; n++){
-//
+      
 //    5.      declare and initialize an array of n processor Queues and any
 //            necessary storage Queues
     Queue[] procQ = new Queue[n+1];
@@ -95,9 +88,8 @@ public class Simulation{
 
 //    6.      while unprocessed jobs remain  {
     while (jobsRemain(procQ[0], m)) {
-//
-//    7.          determine the time of the next arrival or finish event and
-//                update time
+//    7.      determine the time of the next arrival or finish event and
+//            update time
       int arriv = (int)Double.MAX_VALUE;
       if (!procQ[0].isEmpty() && ((Job)procQ[0].peek()).getFinish()==-1) {
         arriv = ((Job)procQ[0].peek()).getArrival();
@@ -119,8 +111,6 @@ public class Simulation{
         time = arriv;
       }
 
-
-//
 //    8.          complete all processes finishing now
       for (int job = 1; job < procQ.length; job++) {
         if (!procQ[job].isEmpty()) {
@@ -134,21 +124,14 @@ public class Simulation{
               if (first.getFinish()==-1)
                 first.computeFinishTime(time);
             }
-
-            // trace.println("time=" + time);
-            // for (int i = 0; i < procQ.length; i++) {
-            //   trace.println(i + ": " + procQ[i]);
-            // }
-            // trace.println();
           }
         }
       }
-//
+
 //    9.          if there are any jobs arriving now, assign them to a processor
 //                Queue of minimum length and with lowest index in the queue array.
         for (int j = 0; j < procQ[0].length(); j++) {
           Job arrive = (Job)procQ[0].peek();
-          // Job next = (Job)procQ[0].next().peek();
           if (arrive.getArrival() == time) {
             int shortest = shortestQ(procQ);
 
@@ -160,23 +143,19 @@ public class Simulation{
             if (first.getFinish()==-1) {
               first.computeFinishTime(time);
             }
-
           }
-      }
-          trace.println("time=" + time);
-          for (int i = 0; i < procQ.length; i++) {
-            trace.println(i + ": " + procQ[i]);
-          }
-          trace.println();
-        // }
+        }
+        trace.println("time=" + time);
+        for (int i = 0; i < procQ.length; i++) {
+          trace.println(i + ": " + procQ[i]);
+        }
+        trace.println();
 
      //    10.     } end loop
     }
-//
+      
 //    11.     compute the total wait, maximum wait, and average wait for
 //            all Jobs, then reset finish times
-
-
 
     int totalWait = 0;
     int maxWait = 0;
@@ -190,22 +169,20 @@ public class Simulation{
     }
     double avgWait = (double)totalWait/m;
 
-      end = "s: ";
-      if (n == 1) {
-        end = ": ";
-      }
-      report.println(n + " processor" + end + " totalWait="+ totalWait
-        + ", maxWait=" + maxWait + ", averageWait="+avgWait);
+    end = "s: ";
+    if (n == 1) {
+      end = ": ";
+    }
+    report.println(n + " processor" + end + " totalWait="+ totalWait
+      + ", maxWait=" + maxWait + ", averageWait="+avgWait);
 
-//
 //    12. } end loop
   }
-//
+
 //    13. close input and output files
   in.close();
   report.close();
   trace.close();
-
 }
   // jobsRemain()
   // returns true if q contains fewer than m completed jobsRemain
@@ -230,6 +207,7 @@ public class Simulation{
     }
     return shortest;
   }
+  
   // arrayIsEmpty()
   // returns true if every processor queue is empty
   private static boolean arrayIsEmpty(Queue[] q) {
